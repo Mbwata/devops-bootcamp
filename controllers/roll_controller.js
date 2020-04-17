56f29b927fffbd8cf9367eea96652ac7ae2a9d45
Matt H <housems@hotmail.com>
2020-04-16 19:31:02 -0500

    I think I got it

--- controllers/roll_controller.js
+++ controllers/roll_controller.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const Die = require('../models/die');
 const { db } = require('../lib/db');
-const { isEnabled } = require('unleash-client');
 const logger = require('../lib/logger');
 
 router.get('/', async (req, res) => {
@@ -12,14 +11,6 @@ router.get('/', async (req, res) => {
   logger.info('Roll value obtained', { numSides: die.sides, rolled: roll });
   let message = `A die with ${die.sides} sides just rolled a ${roll}.`;
   await saveRoll(die.sides, roll);
-
-  if (isEnabled('rollTwo_wsu0000')) {
-    let die2 = new Die(req.query.sides);
-    let roll2 = die2.roll();
-    logger.info('Roll value obtained', { numSides: die2.sides, rolled: roll2 });
-    message = `Two dice each with ${die.sides} sides just rolled a ${roll} and ${roll2}.`;
-    await saveRoll(die2.sides, roll2);
-  }
 
   profiler.done({ message: 'Die roll complete!' });
   res.send(message)
